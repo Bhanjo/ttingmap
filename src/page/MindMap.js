e5ae5e4063b563678ad06118277ce313af5dd2c4
Bhanjo <hanjo1515@naver.com>
2021-12-06 20:30:03 +0900

    리팩토링: 노드 추가 동작 리팩토링

--- src/page/MindMap.js
+++ src/page/MindMap.js
@@ -1,8 +1,7 @@
 import { useCallback, useState, useRef } from 'react';
-import cy from 'react-cytoscapejs';
+import CytoscapeComponent from 'react-cytoscapejs';
 import styled from 'styled-components';
 
-import Cytoscape from '../components/Cytoscape';
 import CytoscapeInsert from '../components/CytoscapeInsert';
 import Navigation from '../components/Navigation';
 
@@ -10,7 +9,6 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   min-height: 500px;
-  /* overflow: hidden; */
   background-color: #c4c4c4;
   display: flex;
   flex-direction: column;
@@ -21,12 +19,21 @@ const Container = styled.div`
   }
 `;
 
+// Cytoscape에 대한 스타일
+const GraphBox = styled(CytoscapeComponent)`
+  width: inherit;
+  height: 80vh;
+  border: 1px solid #000;
+  background-color: #fff;
+`;
+
 const InputGraph = styled.input`
   width: 300px;
 `;
 
 const MindMap = () => {
-  const [elements, setElements] = useState([
+  // 초기값
+  const elements = [
     {
       data: {
         id: 'Node 1',
@@ -39,8 +46,8 @@ const MindMap = () => {
     },
     {
       data: {
-        id: 'Node 2',
-        label: 'Node 2',
+        id: 'Node 3',
+        label: 'Node 3',
       },
       position: {
         x: 540,
@@ -50,11 +57,13 @@ const MindMap = () => {
     {
       data: {
         source: 'Node 1',
-        target: 'Node 2',
-        label: 'edge from node1 to node2',
+        target: 'Node 3',
+        label: 'edge from node1 to node3',
       },
     },
-  ]);
+  ];
+  // cytoscape DOM을 다루기 위한 ref
+  const cyRef = useRef();
 
   // 노드 추가일지 연결일지 판단, 기본값(true)은 노드 추가 기능
   const [insertType, setInsertType] = useState(true);
@@ -62,6 +71,7 @@ const MindMap = () => {
   const [newNode, setNewNode] = useState('');
   const [targetNode, setTargetNode] = useState('');
 
+  // form 내용 변경 감지
   const onChangeNewNode = (e) => {
     setNewNode(e.target.value);
   };
@@ -69,44 +79,37 @@ const MindMap = () => {
     setTargetNode(e.target.value);
   };
 
-  // props로 함수를 전달시 useCallback을 쓰는게 맞는가
-  const onNewGraph = useCallback(
-    (e) => {
-      const item = {
-        data: {
-          id: newNode,
-          label: newNode,
-        },
-        position: {
-          x: 600,
-          y: 600,
-        },
-      };
-      setElements(elements.concat(item));
-      e.preventDefault();
-      setNewNode('');
-    },
-    [elements, newNode],
-  );
+  // 노드 추가 이벤트
+  const onNewGraph = (e) => {
+    const item = {
+      data: {
+        id: newNode,
+        label: newNode,
+      },
+      position: {
+        x: 600,
+        y: 600,
+      },
+    };
+    e.preventDefault();
+    cyRef.current.add(item);
+    setNewNode('');
+  };
 
-  const onConnectGraph = useCallback(
-    (e) => {
-      const newConnect = {
-        data: {
-          source: newNode,
-          target: targetNode,
-          label: `edge from ${newNode} to ${targetNode}`,
-        },
-      };
-      setElements(elements.concat(newConnect));
-      e.preventDefault();
-      setNewNode('');
-      setTargetNode('');
-      // eslint-disable-next-line no-console
-      console.log('노드추가됨');
-    },
-    [elements, newNode, targetNode],
-  );
+  // 노드 연결 이벤트
+  const onConnectGraph = (e) => {
+    const newConnect = {
+      data: {
+        source: newNode,
+        target: targetNode,
+        label: `edge from ${newNode} to ${targetNode}`,
+      },
+    };
+    // setElements(elements.concat(newConnect));
+    e.preventDefault();
+    setNewNode('');
+    setTargetNode('');
+  };
 
   // 입력모드 변경 이벤트
   const changeInsertMode = () => {
@@ -120,10 +123,12 @@ const MindMap = () => {
     <Container>
       <Navigation />
       <h1>자유롭게 생각을 표현하세요!</h1>
-      <Cytoscape
-        elements={elements}
-        // onNewGraph={onNewGraph}
-        // onConnectGraph={onConnectGraph}
+      <GraphBox
+        elements={CytoscapeComponent.normalizeElements(elements)}
+        maxZoom={2}
+        minZoom={0.3}
+        // eslint-disable-next-line no-return-assign
+        cy={(cy) => (cyRef.current = cy)}
       />
       <div>
         <h1>입력 타입 선택</h1>
