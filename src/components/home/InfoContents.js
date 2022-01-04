96463027b5b15a092f2af16f34d5431fe73a63ac
Bhanjo <hanjo1515@naver.com>
2022-01-04 17:46:31 +0900

    수정: 메인 페이지 컴포넌트 구성 분리

---
+++ src/components/home/InfoContents.js
@@ -0,0 +1,110 @@
+import styled from 'styled-components';
+
+import useScrollAnimation from '../../hooks/useScrollAnimation';
+
+const InfoBox = styled.div`
+  background-color: #fff;
+  /* overflow: hidden; */
+`;
+
+const InfoTitle = styled.div`
+  margin: 115px auto;
+  width: 410px;
+  text-align: center;
+`;
+
+const InfoTitleMain = styled.p`
+  font-size: 36px;
+  font-weight: bold;
+  color: #5d5fef;
+`;
+
+const InfoTitleSub = styled.p`
+  font-size: 23px;
+  font-weight: bold;
+  margin-top: 28px;
+`;
+
+const InfoImg = styled.img`
+  width: 594px;
+  height: 362px;
+  border-radius: 18px;
+`;
+
+const InfoList = styled.div`
+  @media only screen and (max-width: 768px) {
+    width: 594px;
+  }
+  transition: 1s ease-out;
+  width: ${(props) => (props.isFullSize ? '1190px' : '594px')};
+  margin: auto auto 240px;
+  div:nth-child(2n) {
+    display: ${(props) => (props.isFullSize ? 'flex' : 'block')};
+    flex-direction: row-reverse;
+  }
+`;
+
+const InfoItem = styled.div`
+  display: ${(props) => (props.isFullSize ? 'flex' : 'block')};
+  margin: 83px auto;
+  overflow: hidden;
+  :first-child,
+  :last-child {
+    margin: 0;
+  }
+`;
+
+const InfoContents = () => {
+  const isFullSize = window.innerWidth >= 1210;
+
+  const animation = {
+    0: useScrollAnimation('up', 1.5),
+    1: useScrollAnimation('up', 1.5, 0.6),
+  };
+
+  const projectIntroduce = [
+    {
+      id: 1,
+      animation: useScrollAnimation(isFullSize ? 'left' : 'up', 1.5, 0.2),
+      projectImg: 'https://via.placeholder.com/594x362',
+      introduceText: '대충설명1',
+    },
+    {
+      id: 2,
+      animation: useScrollAnimation(isFullSize ? 'right' : 'up', 1.5, 0.2),
+      projectImg: 'https://via.placeholder.com/594x362',
+      introduceText: '대충설명2',
+    },
+    {
+      id: 3,
+      animation: useScrollAnimation(isFullSize ? 'left' : 'up', 1.5, 0.2),
+      projectImg: 'https://via.placeholder.com/594x362',
+      introduceText: '대충설명3',
+    },
+    {
+      id: 4,
+      animation: useScrollAnimation(isFullSize ? 'right' : 'up', 1.5, 0.2),
+      projectImg: 'https://via.placeholder.com/594x362',
+      introduceText: '대충설명4',
+    },
+  ];
+
+  return (
+    <InfoBox>
+      <InfoTitle>
+        <InfoTitleMain {...animation[0]}>생각을 펼치고 싶다면?</InfoTitleMain>
+        <InfoTitleSub {...animation[1]}>띵맵으로 쉽고 간편하게!</InfoTitleSub>
+      </InfoTitle>
+      <InfoList isFullSize={isFullSize}>
+        {projectIntroduce.map((introduce) => (
+          <InfoItem {...introduce.animation} key={introduce.id}>
+            <InfoImg src={introduce.projectImg} />
+            <p>{introduce.introduceText}</p>
+          </InfoItem>
+        ))}
+      </InfoList>
+    </InfoBox>
+  );
+};
+
+export default InfoContents;
